3a5923b339e2b1a39ab7ddd45e294b9750f38a7f
maksym1503 <max.bilyi@gmail.com>
2020-11-12 13:04:44 +0100

    Homework #1 DONE!

--- Test.py
+++ Test.py
@@ -1,6 +1,22 @@
-print("Hello!")
+####### Homework 1
 
-a=1
-b=2
-c=a+b
-print(c)
+####### Maksym Bilyi, ID: 101169
+
+# Step 1: declaring variables
+import random as random
+
+random.seed(66627)
+A = random.randrange(0, 20, 1)
+B = random.randrange(30, 40, 1)
+
+print(A)
+print(B)
+
+# Step 2: checking variable types. Result: <class 'int'>
+type(A)
+type(B)
+
+# Step 3: programming prime numbers
+for prm in range(A,B):
+    if all(prm%i!=0 for i in range(2,prm)):
+       print (prm)
